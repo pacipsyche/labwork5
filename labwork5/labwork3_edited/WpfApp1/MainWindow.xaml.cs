162679764f3c6aa712fe10552d1581ced2acba7e
pacipsyche <148044701+pacipsyche@users.noreply.github.com>
2024-10-17 15:38:48 +0700

    add labwork5 part 1

---
+++ labwork5/labwork3_edited/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private int backgroundColorIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeBackground(object sender, RoutedEventArgs e)
+        {
+            switch (backgroundColorIndex)
+            {
+                case 0:
+                    this.Background = new SolidColorBrush(Colors.LightBlue);
+                    statusText.Text = "Цвет фона: Светло-синий";
+                    break;
+                case 1:
+                    this.Background = new SolidColorBrush(Colors.LightGreen);
+                    statusText.Text = "Цвет фона: Светло-зеленый";
+                    break;
+                case 2:
+                    this.Background = new SolidColorBrush(Colors.LightCoral);
+                    statusText.Text = "Цвет фона: Светло-коричневый";
+                    break;
+                case 3:
+                    this.Background = new SolidColorBrush(Colors.LightGoldenrodYellow);
+                    statusText.Text = "Цвет фона: Светло-желтый";
+                    break;
+                default:
+                    this.Background = new SolidColorBrush(Colors.White);
+                    statusText.Text = "Цвет фона: Белый";
+                    break;
+            }
+
+            backgroundColorIndex++;
+            if (backgroundColorIndex > 3)
+            {
+                backgroundColorIndex = 0;
+            }
+        }
+
+        private void ShowDeveloperInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Мирзоев Алишер, 2307б2", "О разработчике", MessageBoxButton.OK, MessageBoxImage.Information);
+            statusText.Text = "Информация о разработчике";
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ labwork5/labwork3_edited/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+            brushSizeSlider.Value = 10;
+
+            colorComboBox.SelectedIndex = 0;
+
+            drawMode.IsChecked = true;
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (colorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string selectedColor = selectedItem.Content.ToString();
+
+                switch (selectedColor)
+                {
+                    case "Красный":
+                        inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+                        break;
+                    case "Зеленый":
+                        inkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+                        break;
+                    case "Синий":
+                        inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+                        break;
+                }
+            }
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Width = brushSizeSlider.Value;
+                inkCanvas.DefaultDrawingAttributes.Height = brushSizeSlider.Value;
+            }
+        }
+
+        private void Mode_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb != null)
+            {
+                switch (rb.Content.ToString())
+                {
+                    case "Рисование":
+                        inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                        break;
+                    case "Редактирование":
+                        inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                        break;
+                    case "Удаление":
+                        inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                        break;
+                }
+            }
+        }
+    }
+}
